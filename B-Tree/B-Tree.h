0a7507e492ff8e5d7b18910c803e28b92705882b
JAYICE <49588871+1185430411@users.noreply.github.com>
2019-07-15 14:39:27 +0800

    Update B-Tree.h

--- B-Tree/B-Tree.h
+++ B-Tree/B-Tree.h
@@ -23,14 +23,12 @@ class BTree {
 public:
 	bool insert(int key);
 	bool mydelete(int key);
-	bool destroy();
 private:
 	int	find_pos(Node* node, int key);	//查找一个节点上key值合适插入的位置
 	bool find_node(int key, getnode* search);	//寻找插入位置信息,返回false说明没有根节点
 	bool _insert(Node* node, int key, int keypos, Node* child = 0);		//在一个节点的keys中添加键值
 	bool split(Node* node, Node*& child);
-	void _destroy(Node* node);
-	bool find_earse(int key, getnode* eraseres);	//返回false查找不存在,
+	bool find_earse(int key, getnode* get);	//返回false查找不存在,
 	bool get_ltree_rnode(Node* start, int keypos, Node*& res);	//得到keypos位置左子树最右节点
 	int	 get_childpos(Node* parent, Node* child);
 	Node* combine(Node* parent, Node* brother, Node* node, int parentKeyPos);	//返回合并之后的节点
@@ -38,7 +36,7 @@ private:
 };
 
 
-int BTree::find_pos(Node*node,int key) {
+int BTree::find_pos(Node*node, int key) {
 	if (node == NULL) {
 		return -1;
 	}
@@ -139,7 +137,7 @@ bool BTree::insert(int key) {
 				break;
 			}
 			//若不是根结点，则把中间元素插入到父结点中
-			else{
+			else {
 
 				node = node->parent;
 				posi = this->find_pos(node, node->key[(M + 1) / 2]);
@@ -150,5 +148,213 @@ bool BTree::insert(int key) {
 			break;
 		}
 	}
+	return true;
+}
+
+
+bool BTree::find_earse(int key, getnode*get) {
+	Node*temp = root;
+	int posi=1;
+	while (true) {
+		for (posi = 1; posi <= temp->keycount&&key < temp->key[posi]; posi++);
+		//若找到了叶子结点，posi元素还是比key答案，则找不到
+		if (temp->leaf&&temp->key[posi] > key) return false;
+		//若for循环的中断是因为确切地找到了key值，则得到了该结点
+		else if (temp->key[posi] == key) {
+			get->node = temp;
+			get->keypos = posi;
+		}
+		//若temp既不是叶子结点，在temp也找不到确切的key值，则说明key在temp的child[posi-1]上
+		temp = temp->child[posi - 1];
+	}
+}
+
+bool BTree::get_ltree_rnode(Node*start, int keypos,Node*&res) {
+	if (start == NULL) return false;	
+	start = start->child[keypos - 1];//   左子树
+	while (!start->leaf) {
+		start = start->child[start->keycount];//最右值
+	}
+	res = start;
+	return true;
+}
+
+int BTree::get_childpos(Node*parent, Node*child) {
+	for (int i = 0; i < parent->keycount; i++) {
+		if (parent->child[i] == child) return i;
+	}
+
+	return -1;	
+}
+
+Node* BTree::combine(Node* parent, Node* brother, Node* node, int parentKeyPos) {
+	int key = parent->key[parentKeyPos];
+	for (int i = parentKeyPos; i < parent->keycount; i++) {
+		parent->key[i] = parent->key[i + 1];
+		parent->child[i] = parent->child[i + 1];
+	}
+	parent->keycount--;
+
+	//父结点的parentKeyPos上的元素（即key）只有两种情况，要么比node的所有元素大（顺序node key brother），要么比brother的所有元素大（顺序brother key node）
+	if (key > node->key[node->keycount]) {
+		node->key[node->keycount + 1] = key;
+		node->child[node->keycount + 1] = brother->child[0];
+		node->keycount++;
+	}
+	else {
+		brother->key[brother->keycount + 1] = key;
+		brother->child[brother->keycount + 1] = node->child[0];
+		brother->keycount++;
+	}
+
+	Node*merge = new Node(node->parent, node->leaf);
+	//若node在key的右边
+	if (node->key[1] > brother->key[brother->keycount]) {
+		merge->child[0] = brother->child[0];
+		for (int i = 1; i <=brother->keycount; i++) {
+			merge->key[i] = brother->key[i];
+			merge->child[i] = brother->child[i];
+			if (!merge->leaf)  merge->child[i]->parent = merge;
+		}
+		for (int i = brother->keycount + 1; i <= node->keycount + brother->keycount; i++) {
+			merge->key[i] = node->key[i - brother->keycount];
+			merge->child[i] = node->child[i - brother->keycount];
+			if (!merge->leaf) merge->child[i]->parent = merge;
+		}
+	}
+
+	//若node在key的左边
+	else if (brother->key[1] > node->key[node->keycount]) {
+		for (int i = 1; i <= node->keycount; i++) {
+			merge->key[i] = node->key[i];
+			merge->child[i] = node->child[i];
+			if (!merge->leaf)  merge->child[i]->parent = merge;
+		}
+		for (int i = node->keycount + 1; i <= brother->keycount + node->keycount; i++) {
+			merge->key[i] = brother->key[i - node->keycount];
+			merge->child[i] = brother->child[i - node->keycount];
+			if (!merge->leaf) merge->child[i]->parent = merge;
+		}
+	}
+
+	merge->keycount = node->keycount + brother->keycount;
+	parent->child[parentKeyPos - 1] = merge;
+	delete node;
+	delete brother;
+	return merge;
+
+}
+
+
+bool BTree::mydelete(int key) {
+	if (this->root == NULL) return  false;
+
+	getnode *get;
+	//若找不到该元素
+	if (this->find_earse(key, get) == false)  return false;
+	Node*node = get->node;
+	int posi = get->keypos;
+	//若不是叶子结点，则用左子树的最右值来取带，再删除左子树最右值
+	if (!node->leaf) {
+		//
+		Node*replace = NULL;
+		this->get_ltree_rnode(node, posi, replace);
+		node->key[posi] = replace->key[replace->keycount];
+		node = replace;//赋值为node，后续则是对replace进行删除
+		replace->keycount--;
+	}
+	else {
+		for (int i = posi; i < node->keycount; i++) {
+			node->key[i] = node->key[i + 1];
+			node->child[i] = node->child[i + 1];
+		}
+		node->keycount--;
+	}
+
+	//对删除之后各结点的元素数量等情况进行处理
+	while (true) {
+		//若是根结点
+		if (node->parent == NULL) {
+			//若即是根结点，又是叶结点
+			if (node->leaf) {
+				root = NULL;
+			}
+			else {
+				root = node->child[0];
+				root->parent = NULL;
+			}
+			return true;
+		}
+
+		//若不是根结点
+		else {
+			//若删除后，结点元素个数小于（M+1）/2-1.
+			if (node->keycount < KEY_MIN) {
+				int posi = this->get_childpos(node->parent, node);
+				auto parent = node->parent;
+
+				//若有左兄弟，并且左兄弟的元素个数大于最小元素个数，则可以向左兄弟借一个
+				if (posi > 0 && parent->child[posi - 1]->keycount> KEY_MIN) {
+					auto leftbro = parent->child[posi - 1];
+					for (int i = node->keycount + 1; i >=1; i--) {
+						node->key[i] = node->key[i - 1];
+						node->child[i] = node->child[i - 1];
+					}
+					node->key[1] = parent->key[posi];
+					node->keycount++;
+					node->child[0] = leftbro->child[leftbro->keycount];  //别忘了
+					node->child[0]->parent = node;
+
+					parent->key[posi] = leftbro->key[leftbro->keycount];
+					leftbro->keycount--;
+					return true;
+				}
+
+				//若存在右兄弟，且右兄弟的元素个数大于最小元素个数，则可以向右兄弟借一个
+				else if (posi < parent->keycount&&parent->child[posi + 1]->keycount>KEY_MIN) {
+					auto rightbro = parent->child[posi + 1];
+
+					node->key[node->keycount + 1] = parent->key[posi + 1];
+					node->keycount++;
+					node->child[node->keycount] = rightbro->child[0];
+					node->child[node->keycount]->parent = node;
+
+
+					parent->key[posi + 1] = rightbro->key[0];
+
+					//在rightbro中删除key[0]，因为它已经被移到parent上了
+					for (int i = 1; i < rightbro->keycount; i++) {
+						rightbro->key[i] = rightbro->key[i + 1];
+						rightbro->child[i] = rightbro->child[i + 1];
+					}
+					rightbro->keycount--;
+					return true;
+				}
+
+
+				//左右兄弟都无法给自己借元素
+				else {
+					//若有左兄弟
+					Node*merge = NULL;
+					if (posi > 0) {
+						auto left = parent->child[posi - 1];
+						merge=this->combine(parent, left, node, posi);
+					}
+					//若有右兄弟
+					else if (posi < parent->keycount) {
+						auto right = parent->child[posi + 1];
+						merge = this->combine(parent, right, node, posi+1);
+					}
+					node = merge->parent;  //上溯
+				}
+			}
+
+			//若删除之后B-Tree仍然平衡，则删除直接结束
+			else {
+				return true;
+			}
+		}
+	}
+
 	return true;
 }
